250f79a8b7651e371d00e31b5293b21cfaed6c8e
Bhavyashree-DC <bhavyadc05@gmail.com>
2023-12-04 11:59:09 +0530

    added new tagName with css styling

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-import savedTasks from './data/tasks.json' assert {type : 'json'}
+import savedTasks from "./data/tasks.json" assert { type: "json"};
 
 // Display Today Date & Time
 
@@ -15,48 +15,57 @@ const timeFormatOptions = {
   hour12: true,
 };
 
+
 const dateFormat = new Intl.DateTimeFormat("en-GB", dateFormatOptions);
 const timeFormat = new Intl.DateTimeFormat("en-GB", timeFormatOptions);
 
-setInterval(() =>{
+setInterval(() => {
 
-let today = new Date();
+  let today = new Date();
 
-document.getElementById("dateValue").innerHTML = dateFormat.format(today);
-document.getElementById("timeValue").innerHTML = timeFormat.format(today);
+  document.getElementById("dateValue").innerHTML = dateFormat.format(today);
+  document.getElementById("timeValue").innerHTML = timeFormat.format(today);
 
 }, 1000)
 
 // Display all TaskLists 
 
-function displayTaskList(value,index){
+function displayTaskList(value, index) {
+  console.log(value)
+  const formatDate = new Intl.DateTimeFormat("en-GB", {
+    month: "short",
+    day: "2-digit",
+  }).format(new Date(value.deadline));
 
-    const formatDate = new Intl.DateTimeFormat("en-GB",{
-      month: "short",
-      day: "2-digit",
-    }).format(new Date(value.deadline));
+  let todayDate = new Date().getDate();
+  let deadlineDate = new Date(value.deadline).getDate();
 
-    let todayDate = new Date().getDate();
-    let deadlineDate = new Date(value.deadline).getDate();
+  let deadlineClass = ''
+  if (value.status === 'completed') {
+    deadlineClass = 'completedDate'
+  } else {
+    deadlineClass = deadlineDate <= todayDate ? 'deadlineDate' : 'month';
+  }
+  console.log('here', deadlineClass)
+  const completedClass = value.status === 'completed' ? 'completed' : '';
+  const tagClass = { 'High': 'high', 'Low': 'low', 'Medium': 'medium' };
 
-    const deadlineClass = deadlineDate === todayDate ? 'deadlineDate': 'month';
-
-    const completedClass = value.status === 'completed' ? 'completed' :'';
-
-    return `
+  return `
     <div class="list-item ${value.status}" data-task-id="${value.id}">
         <input type="checkbox" name="selectItem" id="selectItem${index}" class="selectItems" ${value.status === 'completed' ? 'checked' : ''} >
+        <div class="taskContent">
         <div class="taskDeadLine">
-                <div class="${deadlineClass}">${formatDate.split(" ")[1]}</div>
-                <div class="date">${formatDate.split(" ")[0]}</div>
+        <div class="monthBox ${deadlineClass}">${formatDate.split(" ")[1]}</div>
+        <div class="date">${formatDate.split(" ")[0]}</div>
         </div>
-        <label for="selectItems" class="listDetails"> 
+        <label for="selectItems${index}" class="listDetails"> 
             <h4 class="todoName ${completedClass}">${value.todoName}</h4>
             <p class="todoType">${value.type}</p>
             <div class="tag-div">
-                <span class="todoTag high">${value.tag}</span>
+                <span class="todoTag ${tagClass[value.tag]}">${value.tag}</span>
             </div>
         </label>
+        </div>
         <i class="ri-delete-bin-line"></i>
     </div>
     `
@@ -65,47 +74,36 @@ function displayTaskList(value,index){
 // Add new Todo List
 
 function addNewList() {
-       
-    let addListContainer = document.querySelector(".add-list-container");
-    addListContainer.style.display = "block";
 
-    let addTask = document.getElementById("addBtn");
+  let addListContainer = document.querySelector(".add-list-container");
+  addListContainer.style.display = "block";
 
-    addTask.addEventListener("click", function () {
-      const taskInput = document.getElementById("inputText");
-      const taskType = document.getElementById("projectList");
-      const deadline =  document.getElementById("deadline");
-    
+  let addTask = document.getElementById("addBtn");
+
+  addTask.addEventListener("click", function () {
+    const taskInput = document.getElementById("inputText");
+    const taskType = document.getElementById("projectList");
+    const deadline = document.getElementById("deadline");
+
     if (!taskInput.value) {
       alert("Please enter a valid task");
       return;
-    }   
+    }
 
     let inputValue = taskInput.value;
     let selectedOption = taskType.options[taskType.selectedIndex];
     let selectedValue = selectedOption.value;
     let selectedDate = deadline.value;
 
-    const lowTags = document.getElementById('lowTag');
-    const highTags = document.getElementById('highTag');
+    let selectedTag = document.querySelector('input[name="tagName"]:checked').value;
 
-    let selectedTag ; 
-
-    if(lowTags.checked){
-      selectedTag = lowTags.value;
-    }else if(highTags.checked){
-      selectedTag = highTags.value;
-    }else{
-      selectedTag = "Low";
-    }
-      
     const listITems = {
-        id: taskList.length+1,
-        todoName: inputValue,
-        type: selectedValue,
-        tag: selectedTag,
-        deadline: selectedDate,
-        status: "pending",
+      id: taskList.length + 1,
+      todoName: inputValue,
+      type: selectedValue,
+      tag: selectedTag,
+      deadline: selectedDate,
+      status: "pending",
     };
 
     taskList.unshift(listITems);
@@ -124,119 +122,87 @@ function addNewList() {
 
 // creating array to dynamically insert the values
 
-let taskList = savedTasks
-// [
-//   {
-//        "id":1,
-//        "todoName": "Do yoga",
-//        "type": "Home",
-//        "tag": "High",
-//        "deadline":"2023-11-30",
-//        "status":"completed"
-//    },
-     
-//    {
-//        "id":2,
-//        "todoName": "Stand up call at 1.30PM",
-//        "type": "Work",
-//        "tag": "High",
-//        "deadline":"2023-12-01",
-//        "status":"pending"
-//    }
-// ]
-
-// taskList.push({
-//     id:3,
-//     todoName: "Training Session at 1PM",
-//     type: "Work",
-//     tag: "High",
-//     deadline:'2023-12-01',
-//     status:"pending", 
-// });
+let taskList = savedTasks;
 
 
 // Filtering & sorting task based on status & date 
 
 const tasks = document.getElementById("taskLists");
 
-function renderTask(){
+function renderTask() {
 
-    let newString = "";
+  let newString = "";
 
-    taskList.sort(function (a,b){
-        const aDate = new Date(a.deadline);
-        const bDate = new Date(b.deadline);
-        return aDate - bDate ;
-    });
+  taskList.sort(function (a, b) {
+    const aDate = new Date(a.deadline);
+    const bDate = new Date(b.deadline);
+    return aDate - bDate;
+  });
 
-    const pendingTask = taskList.filter(function(list){
-        return list.status != 'completed' ;
-    });
+  const pendingTask = taskList.filter(function (list) {
+    return list.status != 'completed';
+  });
 
-    const completeTask = taskList.filter(list => list.status === 'completed');
+  const completeTask = taskList.filter(list => list.status === 'completed');
 
-    pendingTask.forEach((list,index) => {
-      newString += displayTaskList(list,index);
+  pendingTask.forEach((list, index) => {
+    newString += displayTaskList(list, index);
 
-    });
+  });
 
-    completeTask.forEach((list,index) => {
-      newString += displayTaskList(list,index);
+  completeTask.forEach((list, index) => {
+    newString += displayTaskList(list, index);
 
-    });
+  });
 
-    tasks.innerHTML = newString;
-
-  
-    // event listener for checkBox , delete icon , close icons
-
-    const checkboxes = document.querySelectorAll('.selectItems');
-    checkboxes.forEach(checkbox => checkbox.addEventListener('change', function() {
-        const parentElement = checkbox.closest('.list-item');
-        const taskId = parentElement.dataset.taskId;
-      
-        let task = taskList.find(t => t.id.toString() === taskId);
-    
-        if (task) {
-            task.status = checkbox.checked ? 'completed' : 'pending';
-            renderTask(); 
-        } else {
-            console.log('Task not found for', taskId);
-        }
-
-    })
-    );
-
-    const addTask = document.getElementById('addNewListBtn')
-    addTask.addEventListener('click', addNewList);
+  tasks.innerHTML = newString;
 
 
-    const addTag = document.querySelectorAll('.add-icons');
-    addTag.forEach(tagLine => tagLine.addEventListener('click',selectTagLine));
+  // event listener for checkBox , delete icon , close icons
 
-  
-    const deleteIcons = document.querySelectorAll('.ri-delete-bin-line');
-    deleteIcons.forEach(icon => {
-      icon.addEventListener('click', function() {
-        const parentElement = icon.closest('.list-item');
-        const taskId = parentElement.dataset.taskId;
+  const checkboxes = document.querySelectorAll('.selectItems');
+  checkboxes.forEach(checkbox => checkbox.addEventListener('change', function () {
+    const parentElement = checkbox.closest('.list-item');
+    const taskId = parentElement.dataset.taskId;
 
-        const index = taskList.findIndex((task) => task.id.toString() === taskId);
-        if (index !== -1) {
-            taskList.splice(index, 1);
-            renderTask();
-        }
+    let task = taskList.find(t => t.id.toString() === taskId);
+
+    if (task) {
+      task.status = checkbox.checked ? 'completed' : 'pending';
+      renderTask();
+    } else {
+      console.log('Task not found for', taskId);
+    }
+
+  })
+  );
+
+  const addTask = document.getElementById('addNewListBtn')
+  addTask.addEventListener('click', addNewList);
+
+
+
+
+  const deleteIcons = document.querySelectorAll('.ri-delete-bin-line');
+  deleteIcons.forEach(icon => {
+    icon.addEventListener('click', function () {
+      const parentElement = icon.closest('.list-item');
+      const taskId = parentElement.dataset.taskId;
+
+      const index = taskList.findIndex((task) => task.id.toString() === taskId);
+      if (index !== -1) {
+        taskList.splice(index, 1);
+        renderTask();
+      }
     })
   });
 
+  const closeIcon = document.getElementById('closeIcon');
+  closeIcon.addEventListener('click', closeAddListContainer);
 
-    const closeIcon = document.getElementById('closeIcon');
-    closeIcon.addEventListener('click',closeAddListContainer);
-       
 }
 
 renderTask();
-
 
 
 // close Add list container when icon is clicked
@@ -247,11 +213,9 @@ function closeAddListContainer() {
 
 // Display radio buttons to select Tag Name 
 
-function selectTagLine(){
-
-  const tags = document.querySelector(".tag-input .tagItems")
-
+document.querySelector(".add-icons").addEventListener("click", (e) => {
+  console.log('dfheof', this)
+  const tags = e.currentTarget.querySelector(".tagItems")
   tags.style.display = "flex";
-  tags.style.marginTop = "20px";
-  tags.style.gap = "10px";
-}
+});
+
